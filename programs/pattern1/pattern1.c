745fd5a911f50b4a9f44ecb14b28dd3f338aaaa0
Mugara250 <mbonyumugara52@gmail.com>
2025-05-28 08:20:11 +0200

    decreasing number pattern

--- programs/pattern1/pattern1.c
+++ programs/pattern1/pattern1.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 
-// Create a function that will handle logic of building the pattern
-void pattern(int number) {
-    for (int i = 0; i < number; i++) {
-        // inner loop to print the line
-        for (int j = 0; j < number - i; j++) {
-            printf("%d ", number - j);
+int main()
+{
+    int number = 8;
+
+    for (int i = 0; i < number; i++) 
+    {
+        for (int j = 0; j < number - i; j++) 
+        {
+            printf("%d", number - j);
         }
         printf("\n");
     }
-}
-
-int main() {
-    // variable to store the number
-    int num;
-
-    printf("A program that will print a certain decreasing pattern based on the given input number\n");
-
-    // getting the number from the user and reading it into memory
-    printf("Enter any number: ");
-    scanf("%d", &num);
-
-    // passing the input number to the pattern function call
-    pattern(num);
     return 0;
 }
