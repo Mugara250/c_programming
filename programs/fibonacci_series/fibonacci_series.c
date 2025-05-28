cfbb0e2df91ef9f4a30a9667b094f17730ba69b2
Mugara250 <mbonyumugara52@gmail.com>
2025-05-28 08:19:16 +0200

    fibonacci series based on the input number of terms

--- programs/fibonacci_series/fibonacci_series.c
+++ programs/fibonacci_series/fibonacci_series.c
@@ -5,12 +5,17 @@ This a program that displays the fibonacci series of  15
 #include <stdio.h>
 
 int main() {
+  int terms;
+
+  printf("Enter the number of terms for the fibonacci series: ");
+  scanf("%d", &terms);
+
   int num1 = 0;
   int num2 = 1;
   int num3 = num1 + num2;
   printf("%d\n%d\n%d\n", num1, num2, num3);
 
-  for (int i = 1; i < 13; i++) {
+  for (int i = 1; i < terms - 2; i++) {
     num1 = num2;
     num2 = num3;
     num3 = num1 + num2;
